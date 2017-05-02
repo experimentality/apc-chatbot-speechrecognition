8f8ee00c220366316aea16006a8d3772d060b9bb
Experimentality <sebas.arango@experimentality.co>
2017-05-02 12:49:19 -0500

    first commit

---
+++ app/static/js/script.js
@@ -0,0 +1,95 @@
+var accessToken = "47bae10e67094366891a6fc22f6fddca";
+var subscriptionKey = "ed4211907a764f7d8d2e3051f198a5ab";
+var baseUrl = "https://api.api.ai/v1/";
+
+$(document).ready(function() {
+  $("#input").keypress(function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      send();
+    }
+  });
+  $("#rec").click(function(event) {
+    switchRecognition();
+  });
+});
+var recognition;
+function startRecognition() {
+  recognition = new webkitSpeechRecognition();
+  recognition.onstart = function(event) {
+    updateRec();
+  };
+  recognition.onresult = function(event) {
+    var text = "";
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        text += event.results[i][0].transcript;
+      }
+      setInput(text);
+    stopRecognition();
+  };
+  recognition.onend = function() {
+    stopRecognition();
+  };
+  recognition.lang = "es-ES";
+  recognition.start();
+}
+
+function stopRecognition() {
+  if (recognition) {
+    recognition.stop();
+    recognition = null;
+  }
+  updateRec();
+}
+function switchRecognition() {
+  if (recognition) {
+    stopRecognition();
+  } else {
+    startRecognition();
+  }
+}
+function setInput(text) {
+  $("#input").val(text);
+  send();
+}
+function updateRec() {
+  $("#rec").text(recognition ? "Parar" : "Hablar");
+}
+
+
+
+function send() {
+  var text = $("#input").val();
+
+//let apiaiRequest = apiAiService.textRequest(txt, {
+//    sessionId: sender
+//    });
+
+  $.ajax({
+    type: "POST",
+    url: baseUrl + "query?v=20150910",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    headers: {
+      "Authorization": "Bearer " + accessToken
+    },
+
+      //JSON.stringify(text);
+    //query: JSON.stringify([ text ]),
+      query: JSON.stringify({ query: text, lang: "es", sessionID: "1234567"}),
+
+
+      success: function(data) {
+      setResponse(JSON.stringify(data, undefined, 2));
+    },
+    error: function() {
+      setResponse("Internal Server Error");
+    }
+  });
+  setResponse("Cargando respuesta...");
+}
+//*/
+
+function setResponse(val) {
+  $("#response").text(val);
+}
